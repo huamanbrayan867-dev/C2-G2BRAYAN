752ca59eecb556ce5814247f361a0bb3d8d8a353
huamanbrayan867-dev <huamanbrayan867@gmail.com>
2025-08-28 16:49:28 -0500

    where go

---
+++ POO_encapsulamiento/src/modelo/Persona.java
@@ -0,0 +1,25 @@
+package modelo;
+
+/**
+ * @author Mg. Daniel Levano
+ */
+public class Persona {
+
+    private String nombre;     private int edad;     private double talla;
+
+    public Persona() {
+    }
+    public void setNombre(String nombre) {         this.nombre = nombre;
+    }
+    public String getNombre() {         return nombre;
+    }
+    public void setEdad(int edad) {         this.edad = edad;
+    }
+    public int getEdad() {         return edad;
+    }
+    public void setTalla(double talla) {         this.talla = talla;
+    }
+    public double getTalla() {
+               return talla;
+    }
+}
